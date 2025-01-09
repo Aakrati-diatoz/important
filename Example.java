dcd06ac28fec82d22c9804966ecb2d8c17e5ba8d
Aakrati Singhal <59551634+Aakrati-diatoz@users.noreply.github.com>
2025-01-09 23:27:17 +0530

    task-ULLWP-7_Story

--- Example.java
+++ Example.java
@@ -1,6 +1,6 @@
 class Example {
 
     public static void main(String[] args){
-        System.out.println("Hello");
+        System.out.println("Pull Request automation should work now");
     }
 }
